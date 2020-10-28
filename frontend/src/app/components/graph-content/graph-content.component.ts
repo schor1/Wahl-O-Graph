446f17bbf82075b3f862852ff860ba6c04a02745
schor1 <37296458+schor1@users.noreply.github.com>
2020-10-28 14:01:19 +0100

    Add files via upload

---
+++ frontend/src/app/components/graph-content/graph-content.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-graph-content',
+  templateUrl: './graph-content.component.html',
+  styleUrls: ['./graph-content.component.css']
+})
+export class GraphContentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
